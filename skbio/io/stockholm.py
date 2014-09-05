bd59f8acff4e8a9253d11c81ddcbd62093a4e960
Jai Ram Rideout <jai.rideout@gmail.com>
2014-09-05 15:24:34 -0700

    Stockholm writer now streams to file
    Instead of building up a dual representation in memory and then dumping to file.

--- skbio/io/stockholm.py
+++ skbio/io/stockholm.py
@@ -66,13 +66,13 @@ def _alignment_to_stockholm(obj, fh):
 
 @register_writer('stockholm', StockholmAlignment)
 def _stockholm_alignment_to_stockholm(obj, fh):
+    # write header
+    fh.write('# STOCKHOLM 1.0\n')
+
     # find length of leader info needed to make file pretty
     # 10 comes from the characters for '#=GF ' and the feature after label
     infolen = max(map(len, obj.ids())) + 10
 
-    GF_lines = []
-    GS_lines = []
-    GC_lines = []
     # NOTE: EVERYTHING MUST BE COERCED TO STR in case int or float passed
     # add GF information if applicable
     if obj.gf:
@@ -88,8 +88,10 @@ def _stockholm_alignment_to_stockholm(obj, fh):
                 tree = obj.gf["NH"] if isinstance(obj.gf["NH"], list) \
                     else [obj.gf["NH"]]
                 for ident, tree in zip(obj.gf["TN"], obj.gf["NH"]):
-                    GF_lines.append(' '.join(["#=GF", "TN", str(ident)]))
-                    GF_lines.append(' '.join(["#=GF", "NH", str(tree)]))
+                    fh.write(' '.join(["#=GF", "TN", str(ident)]))
+                    fh.write('\n')
+                    fh.write(' '.join(["#=GF", "NH", str(tree)]))
+                    fh.write('\n')
             elif feature == "RT":
                 # make sure each reference block stays together
                 # set up lists to zip in case some bits are missing
@@ -104,53 +106,59 @@ def _stockholm_alignment_to_stockholm(obj, fh):
                 rc = obj.gf.get("RC", default_none)
                 # order: RN, RM, RT, RA, RL, RC
                 for n, m, t, a, l, c in zip(rn, rm, rt, ra, rl, rc):
-                    GF_lines.append(' '.join(["#=GF", "RN", n]))
+                    fh.write(' '.join(["#=GF", "RN", n]))
+                    fh.write('\n')
                     if m:
-                        GF_lines.append(' '.join(["#=GF", "RM", str(m)]))
+                        fh.write(' '.join(["#=GF", "RM", str(m)]))
+                        fh.write('\n')
                     if t:
-                        GF_lines.append(' '.join(["#=GF", "RT", str(t)]))
+                        fh.write(' '.join(["#=GF", "RT", str(t)]))
+                        fh.write('\n')
                     if a:
-                        GF_lines.append(' '.join(["#=GF", "RA", str(a)]))
+                        fh.write(' '.join(["#=GF", "RA", str(a)]))
+                        fh.write('\n')
                     if l:
-                        GF_lines.append(' '.join(["#=GF", "RL", str(l)]))
+                        fh.write(' '.join(["#=GF", "RL", str(l)]))
+                        fh.write('\n')
                     if c:
-                        GF_lines.append(' '.join(["#=GF", "RC", str(c)]))
+                        fh.write(' '.join(["#=GF", "RC", str(c)]))
+                        fh.write('\n')
             else:
                 # normal addition for everything else
                 if not isinstance(value, list):
                     value = [value]
                 for val in value:
-                    GF_lines.append(' '.join(["#=GF", feature, str(val)]))
+                    fh.write(' '.join(["#=GF", feature, str(val)]))
+                    fh.write('\n')
 
     # add GS information if applicable
     if obj.gs:
         for feature in obj.gs:
             for seqname in obj.gs[feature]:
-                GS_lines.append(' '.join(["#=GS", seqname, feature,
-                                         str(obj.gs[feature][seqname])]))
+                fh.write(' '.join(["#=GS", seqname, feature,
+                                   str(obj.gs[feature][seqname])]))
+                fh.write('\n')
+
+    # create seq output along with GR info if applicable
+    for label, seq in obj.iteritems():
+        spacer = ' ' * (infolen - len(label))
+        fh.write(spacer.join([label, str(seq)]))
+        fh.write('\n')
+        # GR info added for sequence
+        for feature in viewkeys(obj.gr):
+            value = obj.gr[feature][label]
+            leaderinfo = ' '.join(['#=GR', label, feature])
+            spacer = ' ' * (infolen - len(leaderinfo))
+            fh.write(spacer.join([leaderinfo, value]))
+            fh.write('\n')
 
     # add GC information if applicable
     if obj.gc:
         for feature, value in viewitems(obj.gc):
             leaderinfo = ' '.join(["#=GC", feature])
             spacer = ' ' * (infolen - len(leaderinfo))
-            GC_lines.append(spacer.join([leaderinfo,
-                                         str(obj.gc[feature])]))
+            fh.write(spacer.join([leaderinfo, str(obj.gc[feature])]))
+            fh.write('\n')
 
-    sto_lines = ["# STOCKHOLM 1.0"] + GF_lines + GS_lines
-    # create seq output along with GR info if applicable
-    for label, seq in obj.iteritems():
-        spacer = ' ' * (infolen - len(label))
-        sto_lines.append(spacer.join([label, str(seq)]))
-        # GR info added for sequence
-        for feature in viewkeys(obj.gr):
-            value = obj.gr[feature][label]
-            leaderinfo = ' '.join(['#=GR', label, feature])
-            spacer = ' ' * (infolen - len(leaderinfo))
-            sto_lines.append(spacer.join([leaderinfo, value]))
-
-    sto_lines.extend(GC_lines)
     # add final slashes to end of file
-    sto_lines.append('//\n')
-
-    fh.write('\n'.join(sto_lines))
+    fh.write('//\n')
