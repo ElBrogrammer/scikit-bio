6622656da73a5bef67709212b2f2168923077f25
Jai Ram Rideout <jai.rideout@gmail.com>
2014-09-05 16:24:26 -0700

    Decompose stockholm writer
    Easier to see where each piece of the file is written; shorter functions.

--- skbio/io/stockholm.py
+++ skbio/io/stockholm.py
@@ -66,99 +66,110 @@ def _alignment_to_stockholm(obj, fh):
 
 @register_writer('stockholm', StockholmAlignment)
 def _stockholm_alignment_to_stockholm(obj, fh):
-    # write header
+    # find length of leader info needed to make file pretty. 10 comes from the
+    # characters for '#=GR ' and the feature after the sequence label
+    spacing = max(map(len, obj.ids())) + 10
+
+    _write_header(fh)
+    _write_gf_markup(fh, obj.gf)
+    _write_gs_markup(fh, obj.gs)
+    _write_alignment_and_gr_markup(fh, obj, spacing)
+    _write_gc_markup(fh, obj.gc, spacing)
+    _write_terminator(fh)
+
+
+def _write_header(fh):
     fh.write('# STOCKHOLM 1.0\n')
 
-    # find length of leader info needed to make file pretty
-    # 10 comes from the characters for '#=GF ' and the feature after label
-    infolen = max(map(len, obj.ids())) + 10
 
-    # NOTE: EVERYTHING MUST BE COERCED TO STR in case int or float passed
-    # add GF information if applicable
-    if obj.gf:
-        skipfeatures = set(("NH", "RC", "RM", "RN", "RA", "RL"))
-        for feature, value in obj.gf.items():
-            # list of features to skip and parse special later
-            if feature in skipfeatures:
-                continue
-            # list of features to parse special
-            elif feature == "TN":
-                # trees must be in proper order of identifier then tree
-                ident = value if isinstance(value, list) else [value]
-                tree = obj.gf["NH"] if isinstance(obj.gf["NH"], list) \
-                    else [obj.gf["NH"]]
-                for ident, tree in zip(obj.gf["TN"], obj.gf["NH"]):
-                    fh.write(' '.join(["#=GF", "TN", str(ident)]))
+def _write_gf_markup(fh, gf):
+    # note: everything must be coerced to str in case gf contains ints or
+    # floats
+    skip_features = {'NH', 'RC', 'RM', 'RN', 'RA', 'RL'}
+    for feature, value in gf.items():
+        # list of features to skip and parse special later
+        if feature in skip_features:
+            continue
+        # list of features to parse special
+        elif feature == 'TN':
+            # trees must be in proper order of identifier then tree
+            ident = value if isinstance(value, list) else [value]
+            tree = gf['NH'] if isinstance(gf['NH'], list) else [gf['NH']]
+            for ident, tree in zip(gf['TN'], gf['NH']):
+                fh.write(' '.join(['#=GF', 'TN', str(ident)]))
+                fh.write('\n')
+                fh.write(' '.join(['#=GF', 'NH', str(tree)]))
+                fh.write('\n')
+        elif feature == 'RT':
+            # make sure each reference block stays together
+            # set up lists to zip in case some bits are missing
+            # create rn list if needed
+            default_none = [0]*len(value)
+            rn = gf.get('RN', ['[%i]' % x for x in range(1, len(value)+1)])
+            rm = gf.get('RM', default_none)
+            rt = gf.get('RT', default_none)
+            ra = gf.get('RA', default_none)
+            rl = gf.get('RL', default_none)
+            rc = gf.get('RC', default_none)
+            # order: RN, RM, RT, RA, RL, RC
+            for n, m, t, a, l, c in zip(rn, rm, rt, ra, rl, rc):
+                fh.write(' '.join(['#=GF', 'RN', n]))
+                fh.write('\n')
+                if m:
+                    fh.write(' '.join(['#=GF', 'RM', str(m)]))
                     fh.write('\n')
-                    fh.write(' '.join(["#=GF", "NH", str(tree)]))
+                if t:
+                    fh.write(' '.join(['#=GF', 'RT', str(t)]))
                     fh.write('\n')
-            elif feature == "RT":
-                # make sure each reference block stays together
-                # set up lists to zip in case some bits are missing
-                # create rn list if needed
-                default_none = [0]*len(value)
-                rn = obj.gf.get("RN", ["[%i]" % x for x in
-                                range(1, len(value)+1)])
-                rm = obj.gf.get("RM", default_none)
-                rt = obj.gf.get("RT", default_none)
-                ra = obj.gf.get("RA", default_none)
-                rl = obj.gf.get("RL", default_none)
-                rc = obj.gf.get("RC", default_none)
-                # order: RN, RM, RT, RA, RL, RC
-                for n, m, t, a, l, c in zip(rn, rm, rt, ra, rl, rc):
-                    fh.write(' '.join(["#=GF", "RN", n]))
+                if a:
+                    fh.write(' '.join(['#=GF', 'RA', str(a)]))
                     fh.write('\n')
-                    if m:
-                        fh.write(' '.join(["#=GF", "RM", str(m)]))
-                        fh.write('\n')
-                    if t:
-                        fh.write(' '.join(["#=GF", "RT", str(t)]))
-                        fh.write('\n')
-                    if a:
-                        fh.write(' '.join(["#=GF", "RA", str(a)]))
-                        fh.write('\n')
-                    if l:
-                        fh.write(' '.join(["#=GF", "RL", str(l)]))
-                        fh.write('\n')
-                    if c:
-                        fh.write(' '.join(["#=GF", "RC", str(c)]))
-                        fh.write('\n')
-            else:
-                # normal addition for everything else
-                if not isinstance(value, list):
-                    value = [value]
-                for val in value:
-                    fh.write(' '.join(["#=GF", feature, str(val)]))
+                if l:
+                    fh.write(' '.join(['#=GF', 'RL', str(l)]))
                     fh.write('\n')
-
-    # add GS information if applicable
-    if obj.gs:
-        for feature in obj.gs:
-            for seqname in obj.gs[feature]:
-                fh.write(' '.join(["#=GS", seqname, feature,
-                                   str(obj.gs[feature][seqname])]))
+                if c:
+                    fh.write(' '.join(['#=GF', 'RC', str(c)]))
+                    fh.write('\n')
+        else:
+            # normal addition for everything else
+            if not isinstance(value, list):
+                value = [value]
+            for val in value:
+                fh.write(' '.join(['#=GF', feature, str(val)]))
                 fh.write('\n')
 
-    # create seq output along with GR info if applicable
+
+def _write_gs_markup(fh, gs):
+    for feature in gs:
+        for seqname in gs[feature]:
+            fh.write(' '.join(['#=GS', seqname, feature,
+                               str(gs[feature][seqname])]))
+            fh.write('\n')
+
+
+def _write_alignment_and_gr_markup(fh, obj, spacing):
     for label, seq in obj.iteritems():
-        spacer = ' ' * (infolen - len(label))
+        spacer = ' ' * (spacing - len(label))
         fh.write(spacer.join([label, str(seq)]))
         fh.write('\n')
-        # GR info added for sequence
+
+        # GR markup for sequence
         for feature in viewkeys(obj.gr):
             value = obj.gr[feature][label]
             leaderinfo = ' '.join(['#=GR', label, feature])
-            spacer = ' ' * (infolen - len(leaderinfo))
+            # TODO possible to get a negative length here?
+            spacer = ' ' * (spacing - len(leaderinfo))
             fh.write(spacer.join([leaderinfo, value]))
             fh.write('\n')
 
-    # add GC information if applicable
-    if obj.gc:
-        for feature, value in viewitems(obj.gc):
-            leaderinfo = ' '.join(["#=GC", feature])
-            spacer = ' ' * (infolen - len(leaderinfo))
-            fh.write(spacer.join([leaderinfo, str(obj.gc[feature])]))
-            fh.write('\n')
 
-    # add final slashes to end of file
+def _write_gc_markup(fh, gc, spacing):
+    for feature, value in viewitems(gc):
+        leaderinfo = ' '.join(['#=GC', feature])
+        spacer = ' ' * (spacing - len(leaderinfo))
+        fh.write(spacer.join([leaderinfo, str(gc[feature])]))
+        fh.write('\n')
+
+
+def _write_terminator(fh):
     fh.write('//\n')
